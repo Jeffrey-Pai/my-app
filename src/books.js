99075bff83138bd14b5e01149a5dc295b04f68ff
Jeffrey-Pai <110954961+Jeffrey-Pai@users.noreply.github.com>
2023-05-19 21:35:24 +0800

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+    {
+        img: img1,
+        title: 'The Legend of Zelda',
+        author: 'Piggyback',
+        id: 1
+    },
+    {
+        img: img2,
+        title: 'Atomic Habits',
+        author: 'James Clear',
+        id: 2
+    },
+    {
+        img: img3,
+        title: 'The Covenant of Water',
+        author: 'Abraham Verghese',
+        id: 3
+    },
+]
